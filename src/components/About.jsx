b2733c75480a7f6631f06a82aeeb2a3a79ba40b2
Rama Nadar <ramaraghunathan18@gmail.com>
2022-07-13 13:11:36 +0530

    ready for deployment

---
+++ src/components/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const About = () => {
+
+  
+  return (
+    <div name='about' className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white ' >
+     <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+        </div>
+        <p className='text-xl mt-20'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga amet nesciunt magni iusto odit at mollitia perferendis? Voluptatum modi assumenda rem reiciendis voluptatem ad possimus sed obcaecati praesentium tempora, minima quis magni amet, incidunt quidem cumque tempore dignissimos dolor doloremque nulla iure. Illum omnis quisquam magnam debitis sit asperiores nesciunt.
+        </p>
+        <br/>
+        <p className='text-xl '>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos molestiae dicta earum debitis? Distinctio, nihil natus, accusantium dolores aut porro dolor nostrum et, provident aliquid recusandae. Nesciunt quae, ea rerum natus quis consequuntur hic sit ex illo molestias saepe, possimus quos minus quam laborum quo corporis. Aliquam et sapiente vel?
+        </p>
+     </div>  
+    </div>
+  )
+}
+
+export default About;
