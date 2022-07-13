a8ff29bc37db42a431dd32af08263b89e0f058e3
Rama Nadar <ramaraghunathan18@gmail.com>
2022-07-13 13:52:28 +0530

    commit changes

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -59,7 +59,7 @@ const SocialLinks = () => {
             {links.map(({id, child, href, style, download})=>{
                 return(
                  <li key={id} className={
-                    `flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-cyan-600`+` `+style}>
+                    "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-cyan-600"+" "+style}>
                  <a
                   href={href}
                   className='flex justify-between items-center w-full text-white'
